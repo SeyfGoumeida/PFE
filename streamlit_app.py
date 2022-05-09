48c211765ea87895d8cd2e81c208e447d7df9393
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2022-05-09 16:20:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,6 @@ if (option=="BaseMagasin"):
 		if (OptionStatusMagasin =="FRANCHISE"):
 			c1.write(magasinsFranchise) 
 			optionNomMagasin = c2.selectbox("Focus sur un magasin :",options= list(magasinsFranchise["Nom d'usage"].values)
-			#st.dataframe(df.head(nb))
 			c2.metric(label="NUMBER OF ARTICLES", value=len(magasinsFranchise))
 
 #--------------------------------------------------------------------------------------------------------------------------------
