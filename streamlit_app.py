9687a8e5240dc87146e6d2c478de8dacf4a1f069
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2022-06-06 17:45:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ if (option=="Passage Client"):
 	passageClient =BDDtmpNbClient[["Jour","Date","Code","Magasin","nb_client_trad","nb_client_sco"]]
 	# Group by Day : 
 	passageClient = passageClient.groupby(["Jour","Date","Code","Magasin"]).sum().sort_values(by=['nb_client_trad'], ascending=False).reset_index()
-	passageClient
+	
 	
 	expander = st.expander("", expanded=False)
 	with expander:
@@ -121,5 +121,5 @@ if (option=="Passage Client"):
 		c1.pyplot(fig1)
 		c2.pyplot(fig2)
 
-
+	passageClient
 			
