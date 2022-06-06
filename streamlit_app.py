7eaa6f243f0789b8a38e2c79f83e69f6ebf6a9f6
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2022-06-06 15:32:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,8 @@ if (option=="Passage Client"):
 	#----------------------------------------------------------------
 	BDDtmpNbClient =BDDtmpNbClient.astype({"stoAnabelKey": int,"hour": str})
 	#----------------------------------------------------------------
-	BDDtmpNbClient= BDDtmpNbClient.rename(columns={"creationDate":"Date","stoAnabelKey":"Code"})
+	BDDtmpNbClient= BDDtmpNbClient.rename(columns={"creationDate":"Date"})
+	BDDtmpNbClient= BDDtmpNbClient.rename(columns={"stoAnabelKey":"Code"})
 	passageClient =BDDtmpNbClient[["Jour","Date","Code","Magasin","nb_client_trad","nb_client_sco"]]
 	# Group by Day : 
 	passageClient = passageClient.groupby(["Date","Code","Magasin","Jour"]).sum().sort_values(by=['nb_client_trad'], ascending=False).reset_index()
