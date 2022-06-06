336077d97cbe759e97a03ec3d2e9806c77815088
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2022-06-06 15:59:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ if (option=="Passage Client"):
 	
 	expander = st.expander("", expanded=False)
 	with expander:
-		optionNomMagasin = c2.selectbox('Selectionner un magasin :',options= list(passageClient["Magasin"].values))
+		optionNomMagasin = expander.selectbox('Selectionner un magasin :',options= list(passageClient["Magasin"].values))
 		magasinPassageCleint = passageClient[passageClient['Magasin'] == optionNomMagasin]
 		fig6, ax6 = plt.subplots()
 		ax6.set_title('Box Plot du passage client')
