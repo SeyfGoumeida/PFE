4ce82e40f0b822141ee5e64439862553c87558c8
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2022-06-06 15:32:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,11 +95,11 @@ if (option=="Passage Client"):
 	#----------------------------------------------------------------
 	BDDtmpNbClient =BDDtmpNbClient.astype({"stoAnabelKey": int,"hour": str})
 	#----------------------------------------------------------------
-	BDDtmpNbClient= BDDtmpNbClient.rename(columns={"creationDate":"Date"})
+	#BDDtmpNbClient= BDDtmpNbClient.rename(columns={"creationDate":"Date"})
 	BDDtmpNbClient= BDDtmpNbClient.rename(columns={"stoAnabelKey":"Code"})
-	passageClient =BDDtmpNbClient[["Jour","Date","Code","Magasin","nb_client_trad","nb_client_sco"]]
+	passageClient =BDDtmpNbClient[["Jour","creationDate","Code","Magasin","nb_client_trad","nb_client_sco"]]
 	# Group by Day : 
-	passageClient = passageClient.groupby(["Date","Code","Magasin","Jour"]).sum().sort_values(by=['nb_client_trad'], ascending=False).reset_index()
+	passageClient = passageClient.groupby(["creationDate","Code","Magasin","Jour"]).sum().sort_values(by=['nb_client_trad'], ascending=False).reset_index()
 	passageClient
 	
 	expander = st.expander("", expanded=False)
